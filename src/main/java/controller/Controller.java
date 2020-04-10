f964bc5314609b12e01f0f350f42bfc0e0b905ec
yerin <forjustice9839>
2020-04-10 15:10:19 +0900

    [feat] Controller 구현
     메인과 테이블 번호 입력

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -15,8 +15,38 @@ public class Controller {
     private static final List<Menu> menus = MenuRepository.menus();
 
     public static void run() {
-        OutputView.printTables(tables);
-        int tableNumber = StringUtil.toInteger(InputView.inputTableNumber());
-        OutputView.printMenus(menus);
+        int mainNumber = selectMain();
+        if(mainNumber == 1) {
+            int tableNumber = selectTable();
+            int menuNumber = StringUtil.toInteger(InputView.inputMenuNumber());
+        }
+        if(mainNumber == 2){
+
+        }
+        if(mainNumber == 3){
+
+        }
+    }
+
+    private static int selectMain() {
+        int mainNumber;
+        try {
+            OutputView.printMenus(menus);
+            mainNumber = StringUtil.toInteger(InputView.inputTableNumber());
+        }catch (Exception e){
+            mainNumber = selectTable();
+        }
+        return mainNumber;
+    }
+
+    private static int selectTable() {
+        int tableNumber;
+        try {
+            OutputView.printTables(tables);
+            tableNumber = StringUtil.toInteger(InputView.inputTableNumber());
+        }catch (Exception e){
+            tableNumber = selectTable();
+        }
+        return tableNumber;
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,18 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static int inputTableNumber() {
+    public static String inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
+        return scanner.nextLine().trim();
+    }
+
+    public static String inputMainNumber() {
+        System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.nextLine().trim();
+    }
+
+    public static String inputMenuNumber() {
+        System.out.println("## 등록할 메뉴를 선택하세요.");
+        return scanner.nextLine().trim();
     }
 }
